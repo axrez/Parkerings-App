bda4444c820f1a3d602156758a4e8df51b99ed26
Emil <isdragon112@gmail.com>
2017-12-16 00:30:54 +0100

    Made progress on serverside data processing

--- app.js
+++ app.js
@@ -3,33 +3,45 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+var parkeringsPlads = require('./server/dataBehandling');
+
 const app = express();
 
-const Readline = SerialPort.parsers.Readline;
-const port = new SerialPort('COM7');
-const parser = port.pipe(new Readline());
+// const Readline = SerialPort.parsers.Readline;
+// const port = new SerialPort('COM7');
+// const parser = port.pipe(new Readline());
 
-var oldParkeringsData = [];
+var oldParkeringsData = [ 895, 455, 1345, 499, 1345, 5321]; 
+exports.data = oldParkeringsData;
 var newParkeringsData = [];
-parser.on('data', (data) => {
-	if(Number(data) == Number(999999)){
-		console.log("dataStart");
-		return;
-	}
+// parser.on('data', (data) => {
+// 	if(Number(data) == Number(999999)){
+// 		console.log("dataStart");
+// 		return;
+// 	}
 	
-	if(Number(data) == 888888){		
-		oldParkeringsData = newParkeringsData;
-		newParkeringsData =[];
-		console.log("dataSlut");
-		return;
-	}
+// 	if(Number(data) == 888888){		
+// 		oldParkeringsData = newParkeringsData;
+// 		newParkeringsData =[];
+// 		console.log("dataSlut");
+// 		return;
+// 	}
 	
-	newParkeringsData.push(Number(data));	
-	console.log(data);
-});
+// 	newParkeringsData.push(Number(data));	
+// 	console.log(data);
+// });
 
 
 app.get('/',(req, res) => {
+	// if(!parkeringsPlads){
+	// 	parkeringsPlads = require('./server/dataBehandling');	
+	// }
+	parkeringsPlads.ledigTjek(oldParkeringsData)
+	res.send(parkeringsPlads.getData());
+	console.log("Beep Boop");
+});
+
+app.get('/data',(req, res) => {
 	res.send(oldParkeringsData);
 });
 
@@ -41,6 +53,6 @@ app.get('/length',(req, res) => {
 	res.send(String(oldParkeringsData.length));
 });
 
-app.listen(3200, () => {
-	console.log('Server started on port 3200...');
+app.listen(3001, () => {
+	console.log('Server started on port 3001...');
 });
---
+++ client/src/components/BeepBoop.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class BeepBoop extends React.Component{
+  state = {data: []};
+  constructor(props) {
+    super(props);
+    // state = {data: []};
+    this.componentDidMount.bind(this);
+  };
+
+  componentDidMount() {
+    fetch('/data')
+      .then(res => res.json())
+      .then(data => this.setState({ data }));
+  };
+  
+  render(){
+    return(
+      <div>
+        <p>Sloop doop im be da beep boop </p>
+        <h1>{this.state.data}</h1>
+      </div>
+    );
+  }
+};
+
+export default BeepBoop;
